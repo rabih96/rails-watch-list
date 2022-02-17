030a4c67a0e49c624c12b08578485482f64166d9
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-17 17:04:06 -0500

    Added remove method.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,11 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @movies = @list.movies
   end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    list = List.find(@bookmark[:list_id])
+    redirect_to list_bookmarks_path(list)
+  end
 end
