622b0a1afab9f39598a4eb7319623d627945c764
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-17 16:51:15 -0500

    Added bookmarks preview with styling. Missing creation of new bookmarks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show create new]
+  resources :lists, only: %i[index show create new] do
+    resources :bookmarks, only: %i[index create new]
+  end
+  resources :bookmarks, only: :destroy
 end
