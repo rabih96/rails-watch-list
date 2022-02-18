8370efbfc11b07176765a57b5e9b3d3131664822
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-18 10:42:42 -0500

    Added bookmarking for each list.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,10 +4,32 @@ class BookmarksController < ApplicationController
     @movies = @list.movies
   end
 
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(book_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_bookmarks_path(@list)
+    else
+      render :new
+    end
+  end
+
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     list = List.find(@bookmark[:list_id])
     redirect_to list_bookmarks_path(list)
+  end
+
+  private
+
+  def book_params
+    params.require(:bookmark).permit(%i[movie_id comment])
   end
 end
