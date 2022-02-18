c5f02cd60170ac7b7a61ddf0298065f1e43c7005
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-18 18:42:10 -0500

    Final fix for heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: %i[index show create new] do
+  resources :lists, only: %i[show create new] do
     resources :bookmarks, only: %i[index create new]
   end
   resources :bookmarks, only: :destroy
